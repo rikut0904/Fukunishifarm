5bbc6bfad0241c31aeb0fcd8d4a74ca1b55c95e2
rikut0904 <rkt9tuba4robot@gmail.com>
2026-07-03 11:05:31 +0900

    fix/robot.ts

--- frontend/app/robots.ts
+++ frontend/app/robots.ts
@@ -10,7 +10,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/admin/", "/login", "/migration"],
+        disallow: ["/admin", "/login", "/migration"],
       },
     ],
     sitemap: `${siteBaseUrl}/sitemap.xml`,
